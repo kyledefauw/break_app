c7a70808b4f8ae20db152dec6ba4f68e9cbee2d2
Kyle DeFauw <kyle.defauw@gmail.com>
2015-09-15 23:13:26 -0600

    Add rotating quotes

---
+++ app/assets/javascripts/RotatingQuotes.js
@@ -0,0 +1,10 @@
+var quotes = ['"Drink a glass of water"', '"Step outside"', '"Take 10 deep breaths"', '"Think of something you are grateful for"', '"Do a few stretches"'];
+
+var i = 0;  // the index of the current item to show
+
+setInterval(function() {            // setInterval makes it run repeatedly
+    document
+        .getElementById('quote_array')
+        .innerHTML = quotes[i++];    // get the item and increment i to move to the next
+    if (i == quotes.length) i = 0;   // reset to first element if you've reached the end
+}, 1500);
